a251677a9539b57627f6be958b93731416578089
Josh Elster <josh@liquidelectron.com>
2012-05-09 12:02:08 -0500

    fixed querying and display formatting - actually provides some meaningful information now! performance considerations not addressed in any detail; requires further testing

--- CommandHandlerCodeIssue/CodeIssueProvider.cs
+++ CommandHandlerCodeIssue/CodeIssueProvider.cs
@@ -35,26 +35,38 @@ namespace CommandHandlerCodeIssue
             if (document.Project == null) return null;
 
             var classNode = (ClassDeclarationSyntax) node;
-            if (classNode.BaseListOpt == null)
-                return null;
+
+            if (classNode.BaseListOpt == null) return null;
 
             var commandHandlerVisitor = new CommandHandlerSyntaxVisitor();
 
             var baseTypes = commandHandlerVisitor.Visit(classNode);
-            
+
             if (baseTypes == null || !baseTypes.Any()) return null;
 
-            var allCommandHandlersInProject = document.Project.GetCompilation(cancellationToken).SyntaxTrees
-                    .SelectMany(x => x.Root.DescendentNodes().OfType<ClassDeclarationSyntax>().Where(s => s.BaseListOpt != null && commandHandlerVisitor.Visit(s).Any()))
-                    .Select(x => new { classDec = x, handles = commandHandlerVisitor.Visit(x) }).ToList();
+            var allCommandHandlersInProject = (from allTrees in document.Project.GetCompilation(cancellationToken).SyntaxTrees
+                                                from classes in allTrees.Root.DescendentNodes().OfType<ClassDeclarationSyntax>()
+                                                    where commandHandlerVisitor.Visit(classes).Any()
+                                               select new KeyValuePair<ClassDeclarationSyntax, IEnumerable<GenericNameSyntax>>(classes, commandHandlerVisitor.Visit(classes))).ToList();
+            var dic = allCommandHandlersInProject.ToDictionary(x => x.Key, pair => pair.Value);
 
-            if (!allCommandHandlersInProject.Any() || !allCommandHandlersInProject.Select(x => x.classDec).Contains(classNode)) return null;
+            if (!dic.Any() || !dic.ContainsKey(classNode)) return null;
 
-            var dupes = allCommandHandlersInProject.SelectMany(x => x.handles, (c, s) => s.GetText()).FindDuplicates();
+            var dupes = dic.SelectMany(x => x.Value, (c, s) => s.GetText()).FindDuplicates().ToList();
 
-            return dupes.Any() ? 
-                dupes.Select(x => new CodeIssue(CodeIssue.Severity.Warning, classNode.Identifier.FullSpan, x)) 
-                : null;
+            if (dupes.Any())
+            {
+                var desc = "{0} is implemented by multiple handlers:{1}{2}";
+                var issues = new List<CodeIssue>();
+                foreach (var dupe in dupes)
+                {
+                    var listing = FormatHandlerListing(dupe, allCommandHandlersInProject);
+                    var text = string.Format(desc, dupe, Environment.NewLine, string.Join(Environment.NewLine, listing));
+                    issues.Add(new CodeIssue(CodeIssue.Severity.Warning, classNode.Identifier.FullSpan, text));
+                }
+                return issues;
+            }
+            return null;
 
             //if (allCommandHandlersInProject.Contains(node as ClassDeclarationSyntax))
             //{
@@ -62,6 +74,14 @@ namespace CommandHandlerCodeIssue
             //}
         }
 
+        private static IEnumerable<string> FormatHandlerListing(string genericInterfaceString, IEnumerable<KeyValuePair<ClassDeclarationSyntax, IEnumerable<GenericNameSyntax>>> handlers)
+        {
+            return
+                handlers.Where(y => y.Value.Any(v => v.GetFullText().Contains(genericInterfaceString))).Select(
+                    x => x.Key.Identifier.GetFullText());
+        }
+
+         
         public IEnumerable<CodeIssue> GetIssues(IDocument document, CommonSyntaxToken token, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
@@ -81,7 +101,7 @@ namespace CommandHandlerCodeIssue
 
         protected override IEnumerable<GenericNameSyntax> VisitClassDeclaration(ClassDeclarationSyntax node)
         {
-            return node.BaseListOpt != null ? node.BaseListOpt.Types.OfType<GenericNameSyntax>().Where(x => x.PlainName == CommandHandlerInterfaceName) : null;
+            return node.BaseListOpt != null ? node.BaseListOpt.Types.OfType<GenericNameSyntax>().Where(x => x.PlainName == CommandHandlerInterfaceName) : Enumerable.Empty<GenericNameSyntax>();
         }
         
     }
